7c2bf5c750d39276d639fa6bdcc8c9d4931e6242
JuliaCicer13 <137887271+JuliaCicer13@users.noreply.github.com>
2025-04-08 02:27:07 +0300

    o

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,11 @@
 import css from './styles/App.module.css';
-import { Route, Routes } from "react-router-dom";
-import Navigation from "../components/Navigation.jsx"
-import { Suspense, lazy } from 'react';
-import MovieCast from "../components/MovieCast.jsx";
-import MovieReviews from "../components/MovieReviews.jsx";
-const HomePage = lazy(() =>  import("../pages/HomePage"));
-const MovieDetailsPage = lazy(() => import("../pages/MovieDetailsPage"));
-const MoviesPage = lazy(() => import("../pages/MoviesPage.jsx"));
-const NotFoundPage = lazy(() => import("../pages/NotFoundPage"));
-
+import Navigation from "../components/Navigation";
 export default function App() {
 
   /* Остальной код */
   return (
     <div className={css.container}>
-      <Navigation />
-      <Suspense fallback={<div>Downloed...</div>}>
-     <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/movies' element={<MoviesPage />} />
-          <Route path='/movies/:movieId' element={<MovieDetailsPage />}>
-             <Route path="cast" element={<MovieCast />} />
-             <Route path="reviews" element={<MovieReviews />} />
-          </Route>
-          <Route path="*" element={<NotFoundPage />} />
-     </Routes>
-      </Suspense>
-    
+      <Navigation/>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-
 
 // Імпорт стилів нормалізації
 import "modern-normalize";
 import App from "./components/App.jsx";
 import "./index.css";
+// 1. Імпортуємо провайдер
+import { Provider } from 'react-redux'
+// 2. Імпортуємо створений раніше стор
+import { store } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+   <Provider store={store}>
+	    <App />
+	  </Provider>
   </React.StrictMode>
 );
